3d3624b407f75b00844c7a964bd4deaf7c9ffa96
hilalozcanyavuz <113976776+hilalozcanyavuz@users.noreply.github.com>
2022-09-21 18:36:05 +0300

    Add files via upload

---
+++ loopDemo/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		//for
+		//i=1 sayaç(başlangıç noktam)
+		//çift sayılar
+		for (int i = 2; i < 10; i+=2) {
+		    System.out.println(i);	
+		}
+		System.out.println("Döngü Bitti");
+
+	}
+
+}
